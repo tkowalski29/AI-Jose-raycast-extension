b62d3c5066357014519c45f14db99be49dba8b42
Tomasz Kowalski <tkowalski@iseno.net>
2024-07-03 20:38:22 +0200

    change catalog

--- src/hook/ai/llm.ts
+++ src/hook/ai/llm.ts
@@ -1,8 +1,8 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { OpenAIEmbeddings } from "@langchain/openai";
-import { GetApiOpenAiKey } from "../type/config";
 import { Toast } from "@raycast/api";
-import { TalkType } from "../type/talk";
+import { GetApiOpenAiKey } from "../../type/config";
+import { TalkType } from "../../type/talk";
 
 export const Call = async (
   chat: TalkType,
